4de926ea9ea522d919b154c91f0e494fc522f5c4
kevin alonso <k.alonso@hokoha.com>
2025-03-31 16:26:23 +0200

    adresse

--- src/map.js
+++ src/map.js
@@ -2,7 +2,7 @@
 
 export function initMap() {
     
-    var map = L.map('map').setView([48.013438, -0.816955], 13); // Coordonnées de Paris
+    var map = L.map('map').setView([48.007438857169625, -0.8166790008544923], 13); // Coordonnées de Paris
 
     // Ajouter le fond de carte OpenStreetMap
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,7 +12,7 @@ export function initMap() {
 
     // Ajouter un marqueur
     var marker = L.marker([48.013438, -0.816955]).addTo(map)
-        .bindPopup("<b>Je suis ici !</b><br>Bienvenue sur ma carte.")
+        .bindPopup("Je suis ici !")
         .openPopup(); // Affiche la popup au chargement
 
         setTimeout(() => {
